95466163553d1685402b3e92a924b20a2f3c45f9
Hari Krishnan <harikrishnans3010@gmail.com>
2020-08-25 22:05:14 +0530

    third commit

--- db.js
+++ db.js
@@ -1,62 +1,134 @@
-var pg = require('pg');
+var pg = require("pg");
 
-var async = require('async');
+var async = require("async");
+const { promises } = require("fs");
 
 // Connect to the "bank" database.
 var config = {
-    user: 'maxroach',
-    host: 'localhost',
-    database: 'bank',
-    port: 26257
+  user: "maxroach",
+  host: "localhost",
+  database: "bank",
+  port: 26257,
 };
 
 // Create a pool.
 var pool = new pg.Pool(config);
-
-const saveTodb = (payload) => {
+const retrieveSender = (payload) => {
+  return new Promise((resolve, reject) => {
     pool.connect(function (err, client, done) {
-
-        // Close communication with the database and exit.
-        var finish = function () {
-            done();
-            process.exit();
-        };
-    
-        if (err) {
-            console.error('could not connect to cockroachdb', err);
-            finish();
+      if (err) {
+        console.error("could not connect to cockroachdb", err);
+      }
+      async.waterfall(
+        [
+          function (next) {
+            client.query(
+              `select message from sender where sender=$1 and date between $2 and $3 limit 2 offset $4`,
+              [
+                payload.name,
+                payload.start,
+                payload.end,
+                parseInt(payload.counter) * 2,
+              ],
+              next
+            );
+          },
+        ],
+        function (err, results) {
+          if (err) {
+            console.error(
+              "Error inserting into and selecting from accounts: ",
+              err
+            );
+          }
+          resolve(results.rows);
         }
-        async.waterfall([
-                function (next) {
-                    client.query('CREATE TABLE IF NOT EXISTS sender (sender STRING, message STRING);', next);
-                },
-                function (results, next) {
-                    client.query('CREATE TABLE IF NOT EXISTS receiver (receiver STRING, message STRING);', next);
-                },
-                function (results, next) {
-                    client.query('INSERT INTO sender (sender, message) VALUES ($1, $2)',[payload.sender,payload.message], next);
-                },
-                function (results, next) {
-                    client.query('INSERT INTO receiver (receiver, message) VALUES ($1, $2)',[payload.receiver,payload.message], next);
-                },
-                function (results, next) {
-                    client.query('SELECT * FROM sender;', next);
-                },
-            ],
-            function (err, results) {
-                if (err) {
-                    console.error('Error inserting into and selecting from accounts: ', err);
-                    finish();
-                }
-    
-                console.log('Initial balances:');
-                results.rows.forEach(function (row) {
-                    console.log(row);
-                });
-    
-                finish();
-            });
+      );
     });
-}
+  });
+};
+const retrieveReceiver = (payload) => {
+  return new Promise((resolve, reject) => {
+    pool.connect(function (err, client, done) {
+      if (err) {
+        console.error("could not connect to cockroachdb", err);
+      }
+      async.waterfall(
+        [
+          function (next) {
+            client.query(
+              `select message from receiver where receiver=$1 and date between $2 and $3 limit 2 offset $4`,
+              [
+                payload.name,
+                payload.start,
+                payload.end,
+                parseInt(payload.counter) * 2,
+              ],
+              next
+            );
+          },
+        ],
+        function (err, results) {
+          if (err) {
+            console.error(
+              "Error inserting into and selecting from accounts: ",
+              err
+            );
+          }
+          resolve(results.rows);
+        }
+      );
+    });
+  });
+};
+const saveTodb = (payload) => {
+  pool.connect(function (err, client, done) {
+    if (err) {
+      console.error("could not connect to cockroachdb", err);
+    }
+    async.waterfall(
+      [
+        function (next) {
+          client.query(
+            "CREATE TABLE IF NOT EXISTS sender (ID SERIAL PRIMARY KEY,sender STRING, message STRING,date STRING);",
+            next
+          );
+        },
+        function (results, next) {
+          client.query(
+            "CREATE TABLE IF NOT EXISTS receiver (ID SERIAL PRIMARY KEY,receiver STRING, message STRING,date STRING);",
+            next
+          );
+        },
+        function (results, next) {
+          var date = new Date();
+          client.query(
+            "INSERT INTO sender (sender, message ,date) VALUES ($1, $2, $3)",
+            [payload.sender, payload.message, date],
+            next
+          );
+        },
+        function (results, next) {
+          client.query(
+            "INSERT INTO receiver (receiver, message,date) VALUES ($1, $2, $3)",
+            [payload.receiver, payload.message, new Date()],
+            next
+          );
+        },
+      ],
+      function (err, results) {
+        if (err) {
+          console.error(
+            "Error inserting into and selecting from accounts: ",
+            err
+          );
+        }
+        console.log(results);
+      }
+    );
+  });
+};
 
-exports.saveTodb=saveTodb;
+exports.saveTodb = saveTodb;
+exports.retrieveSender = retrieveSender;
+exports.retrieveReceiver = retrieveReceiver;
--- server.js
+++ server.js
@@ -1,30 +1,195 @@
-var functions= require('./functions.js');
-
-const Hapi = require("hapi")
-const joi = require("joi")
+var functions = require("./functions.js");
+var db = require("./db");
+const Hapi = require("@hapi/hapi");
+const Joi = require("@hapi/joi");
+const redis = require("redis");
+const client = redis.createClient();
+const { promisify } = require("util");
+const { join } = require("path");
+client.on("error", function (error) {
+  console.error(error);
+});
 const server = new Hapi.server({
-    host:'localhost',
-    port:5000
-})
+  host: "localhost",
+  port: 5000,
+});
+server.route({
+  method: "POST",
+  path: "/retrieveSender",
+  handler: async (req, res) => {
+    var { name, start, end, counter, cached } = req.payload;
+    const getAsynckeys = promisify(client.keys).bind(client);
+    const getAsync = promisify(client.lrange).bind(client);
+    if (!cached) client.del("samp");
+    return getAsynckeys("samp").then((re) => {
+      if (re.length !== 0) {
+        ret = [];
+        return getAsync("samp", counter * 2, counter * 2 + 1).then((re) => {
+          for (let k = 0; k < re.length; k++) ret.push(re[k]);
+          return ret;
+        });
+      }
+      start = new Date(start);
+      end = new Date(end);
+      var key =
+        name +
+        ":" +
+        start.getDate() +
+        "" +
+        start.getMonth() +
+        "" +
+        start.getFullYear();
+
+      return getAsynckeys(key).then((re) => {
+        if (re.length !== 0) {
+          ret = [];
+          for (i = start.getDate(); i <= end.getDate(); i++) {
+            let key =
+              name + ":" + i + "" + start.getMonth() + "" + start.getFullYear();
+            getAsync(key, 0, -1).then(async (re) => {
+              if (re.length !== 0) {
+                for (j = 0; j < re.length; j++) client.rpush("samp", re[j]);
+              }
+            });
+          }
+          return getAsync("samp", counter * 2, counter * 2 + 1).then((re) => {
+            if (re.length !== 0) {
+              for (let k = 0; k < re.length; k++) ret.push(re[k]);
+              return ret;
+            }
+            return ret;
+          });
+        }
+        return db.retrieveSender(req.payload).then((re) => {
+          ret = [];
+          for (var i = 0; i < re.length; i++) {
+            // client.rpush(key, re[i].message);
+            ret.push(re[i].message);
+          }
+          return ret;
+        });
+      });
+    });
+    //   })
+    //   .catch(console.error);
+  },
+});
 
 server.route({
-    method:'POST',
-    path:'/{sender}',
-    handler:(req,res)=> {
-        console.log(req.payload);
-        const schema = joi.object().keys({
-            sender:joi.string().required(),
-            receiver:joi.string().required(),
-            message:joi.string().required()
+  method: "POST",
+  path: "/retrieveReceiver",
+  handler: async (req, res) => {
+    var { name, start, end, counter, cached } = req.payload;
+    const getAsynckeys = promisify(client.keys).bind(client);
+    const getAsync = promisify(client.lrange).bind(client);
+    if (!cached) client.del("samp1");
+
+    return getAsynckeys("samp1").then((re) => {
+      if (re.length !== 0) {
+        ret = [];
+        return getAsync("samp1", counter * 2, counter * 2 + 1).then((re) => {
+          for (let k = 0; k < re.length; k++) ret.push(re[k]);
+          return ret;
         });
-        const validate=schema.validate(req.payload,(err,res)=>{
-            if(err) {
-                console.log("error");
+      }
+      start = new Date(start);
+      end = new Date(end);
+      var key =
+        name +
+        "1:" +
+        start.getDate() +
+        "" +
+        start.getMonth() +
+        "" +
+        start.getFullYear();
+
+      return getAsynckeys(key).then((re) => {
+        if (re.length !== 0) {
+          ret = [];
+          for (i = start.getDate(); i <= end.getDate(); i++) {
+            let key =
+              name +
+              "1:" +
+              i +
+              "" +
+              start.getMonth() +
+              "" +
+              start.getFullYear();
+            getAsync(key, 0, -1).then((re) => {
+              if (re.length !== 0) {
+                for (j = 0; j < re.length; j++) client.rpush("samp1", re[j]);
+              }
+            });
+          }
+          return getAsync("samp1", counter * 2, counter * 2 + 1).then((re) => {
+            if (re.length !== 0) {
+              for (let k = 0; k < re.length; k++) ret.push(re[k]);
+              return ret;
             }
+            return ret;
+          });
+        }
+        return db.retrieveReceiver(req.payload).then((re) => {
+          ret = [];
+          for (var i = 0; i < re.length; i++) {
+            // client.rpush(key, re[i].message);
+            ret.push(re[i].message);
+          }
+          return ret;
         });
-        functions.pushToQueue(req.payload);
-        return null; 
-}
+      });
+    });
+    //   })
+    //   .catch(console.error);
+  },
+});
+
+server.route({
+  method: "POST",
+  path: "/{sender}",
+  handler: (req, res) => {
+    console.log(req.payload);
+    var date1 = new Date();
+    date2 = new Date(date1.getTime() - 7 * 1000 * 3600 * 24);
+    date2 = date2.getDate() + "" + date2.getMonth() + "" + date2.getFullYear();
+    console.log(date2);
+
+    const getAsync = promisify(client.keys).bind(client);
+    getAsync(`${req.payload.sender}:${date2}`).then((re) => {
+      // console.log(re);
+      if (re.length !== 0) client.del(re[0]);
+    });
+    client.rpush(
+      `${
+        req.payload.sender
+      }:${date1.getDate()}${date1.getMonth()}${date1.getFullYear()}`,
+      req.payload.message
+    );
+    getAsync(`${req.payload.receiver}1:${date2}`).then((re) => {
+      // console.log(re);
+      if (re.length !== 0) client.del(re[0]);
+    });
+    client.rpush(
+      `${
+        req.payload.receiver
+      }1:${date1.getDate()}${date1.getMonth()}${date1.getFullYear()}`,
+      req.payload.message
+    );
+    functions.pushToQueue(req.payload);
+    return null;
+  },
+  options: {
+    validate: {
+      params: Joi.object({
+        sender: Joi.string(),
+      }),
+      payload: Joi.object({
+        sender: Joi.string().required(),
+        receiver: Joi.string().required(),
+        message: Joi.string().required(),
+      }),
+    },
+  },
 });
 
 server.start();
